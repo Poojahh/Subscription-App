4bcfae6767f915ab00d1c6f276c6e912d342557e
Poojahh <poojakrishnan1104@gmail.com>
2024-06-25 13:00:22 +0530

    Subscription app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubscribersPage from './components/AddSubscribersPage';
+import View from './components/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddSubscribersPage/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddSubscribersPage/>}/>
+      <Route path="/view" element={<View/>}/>
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/View.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div>
+    <div className="container">
+      <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl no</th>
+      <th scope="col">Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Tony</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Tony</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default View
