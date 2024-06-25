a5a96e8c39e605181281ebbf8098642c764e2ffc
Poojahh <poojakrishnan1104@gmail.com>
2024-06-25 12:26:21 +0530

    Subscription app

---
+++ src/components/AddSubscribersPage.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const AddSubscribersPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Subscriber name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Phone no</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Address</label>
+                        <textarea name="" id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Pincode</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">District</label>
+                        <select name="" id="" className="form-control">
+                            <option value="Select">Select</option>
+                            <option value="Kollam">Kollam</option>
+                            <option value="TVM">TVM</option>
+                            <option value="Kannur">Kannur</option>
+                            <option value="Alappuzha">Alappuzha</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Village office</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddSubscribersPage
