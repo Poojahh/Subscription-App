9712d7aeffa2beb95116d9c7e072b1bdd3db6993
Poojahh <poojakrishnan1104@gmail.com>
2024-06-25 13:16:12 +0530

    Subscriber app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddSubscribersPage from './components/AddSubscribersPage';
 import View from './components/View';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchSubscribersPage from './components/SearchSubscribersPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<AddSubscribersPage/>}/>
       <Route path="/view" element={<View/>}/>
+      <Route path="/search" element={<SearchSubscribersPage/>}/>
 
     </Routes>
     </BrowserRouter>
